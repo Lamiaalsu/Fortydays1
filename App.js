faca2286c065345f60d0196e81e4916ba932ab9c
Lamiaalsu <53065078+Lamiaalsu@users.noreply.github.com>
2019-07-23 07:29:04 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+           <code>About us</code> 
+          <h3>Group leader: </h3>  <h3>Group member 1:</h3> <h3>Group member 2:</h3>
+          <h4>Lamia</h4>            <h4>Haneen</h4>          <h4>Afrah</h4>
+          <h5>we are training in fortydays</h5>
+        </p>
+        
+          
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
